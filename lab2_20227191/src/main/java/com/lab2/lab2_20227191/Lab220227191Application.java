778b82ef8bbea7e5b4ac6bfb036672af6958e190
jasnai10 <a20223806@pucp.edu.pe>
2026-09-03 20:21:55 -0500

    inicio del proyecto

---
+++ lab2_20227191/src/main/java/com/lab2/lab2_20227191/Lab220227191Application.java
@@ -0,0 +1,13 @@
+package com.lab2.lab2_20227191;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220227191Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220227191Application.class, args);
+    }
+
+}
---
+++ lab2_20227191/src/test/java/com/lab2/lab2_20227191/Lab220227191ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab2.lab2_20227191;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220227191ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
